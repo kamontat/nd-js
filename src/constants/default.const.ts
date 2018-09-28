5565ee2c4de72148fe5615a992ce9fcba7000680
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-09-28 12:53:22 +0700

    [refactor] Update function 1 line

--- src/constants/default.const.ts
+++ src/constants/default.const.ts
@@ -1,32 +1,19 @@
 export let DEFAULT_LOGGER_LEVEL = "info";
-export const BeChangeLevel = (level: string) => {
-  DEFAULT_LOGGER_LEVEL = level;
-};
+export const BeChangeLevel = (level: string) => (DEFAULT_LOGGER_LEVEL = level);
 
 export let DEFAULT_COLOR = true;
-export const BeColor = (color: boolean) => {
-  DEFAULT_COLOR = color;
-};
+export const BeColor = (color: boolean) => (DEFAULT_COLOR = color);
 
 export let DEFAULT_QUIET = false;
-export const BeQuiet = () => {
-  DEFAULT_QUIET = true;
-};
+export const BeQuiet = () => (DEFAULT_QUIET = true);
 
 export let DEFAULT_LOG_FILE_EXIST = true;
-export const BeLog = (has: boolean) => {
-  DEFAULT_LOG_FILE_EXIST = has;
-};
+export const BeLog = (has: boolean) => (DEFAULT_LOG_FILE_EXIST = has);
 
 export let DEFAULT_LOG_FOLDER = "/tmp/nd";
-export const BeLoggerTo = (folder: string) => {
-  DEFAULT_LOG_FOLDER = folder;
-};
+export const BeLoggerTo = (folder: string) => (DEFAULT_LOG_FOLDER = folder);
 
 export let DEFAULT_LOG_TYPE: "long" | "short" = "short";
-export let BeShortOutput = () => {
-  DEFAULT_LOG_TYPE = "short";
-};
-export let BeLongOutput = () => {
-  DEFAULT_LOG_TYPE = "long";
-};
+export let BeShortOutput = () => (DEFAULT_LOG_TYPE = "short");
+
+export let BeLongOutput = () => (DEFAULT_LOG_TYPE = "long");
