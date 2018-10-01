032a86b6159953d24bcfc54310589189974b342c
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-10-02 01:18:15 +0700

    [refactor] Update code more readable

--- src/constants/color.const.ts
+++ src/constants/color.const.ts
@@ -1,26 +1,8 @@
 import chalk from "chalk";
 import { ColorType } from "../models/Color";
 import moment, { isDate, isMoment, Moment } from "moment";
-import { MakeReadableNumberArray, CheckIsNumber, CheckIsExist } from "../helpers/helper";
-
-const TransferNothing = (v: any) => v;
-const TransferAsDate = (v: any) => {
-  const settings = {
-    sameDay: "[วันนี้]",
-    lastDay: "[เมื่อวาน]",
-    lastWeek: "[วัน]dddd[ที่แล้ว]",
-    sameElse: "DD/MM/YYYY"
-  };
-  if (isMoment(v)) return (<Moment>v).calendar(undefined, settings) || "";
-  else if (isDate(v)) return moment(<Date>v).calendar(undefined, settings) || "";
-  return v;
-};
-const TransferReadableList = (v: Array<any>) => {
-  if (v.every(v => CheckIsNumber(v) !== null)) {
-    return MakeReadableNumberArray(v);
-  }
-  return v.toString();
-};
+import { CheckIsNumber, CheckIsExist } from "../helpers/helper";
+import { TransferAsDate, TransferNothing, TransferReadableList, noValidator } from "../helpers/color";
 
 export const TITLE_COLOR = chalk.green;
 export const NAME_COLOR = chalk.red.underline;
@@ -39,10 +21,10 @@ export const STRING_COLOR = chalk.reset;
 export const UNDEFINED_COLOR = chalk.reset;
 
 export const CONST_DEFAULT_COLORS = {
-  Title: new ColorType("title", () => false, TITLE_COLOR, TransferNothing),
-  Name: new ColorType("name", () => false, NAME_COLOR, TransferNothing),
-  ChapterName: new ColorType("chapter name", () => false, CHAPTER_NAME_COLOR, TransferNothing),
-  ChapterNumber: new ColorType("chapter number", () => false, CHAPTER_NUMBER_COLOR, TransferNothing),
+  Title: new ColorType("title", noValidator, TITLE_COLOR, TransferNothing),
+  Name: new ColorType("name", noValidator, NAME_COLOR, TransferNothing),
+  ChapterName: new ColorType("chapter name", noValidator, CHAPTER_NAME_COLOR, TransferNothing),
+  ChapterNumber: new ColorType("chapter number", noValidator, CHAPTER_NUMBER_COLOR, TransferNothing),
   ChapterList: new ColorType("chapter list", obj => obj instanceof Array, CHAPTER_NUMBERS_COLOR, TransferReadableList),
   Date: new ColorType(
     "date",
@@ -52,9 +34,9 @@ export const CONST_DEFAULT_COLORS = {
     DATE_TODAY_COLOR,
     (v: Moment) => v.isSame(moment(), "day")
   ),
-  Location: new ColorType("location", () => false, LOCATION_COLOR, TransferNothing),
+  Location: new ColorType("location", noValidator, LOCATION_COLOR, TransferNothing),
   Link: new ColorType("link", obj => obj instanceof URL, LINK_COLOR, TransferNothing),
   Number: new ColorType("number", obj => CheckIsNumber(obj.toString()), NUMBER_COLOR, TransferNothing),
-  String: new ColorType("string", () => false, STRING_COLOR, TransferNothing),
+  String: new ColorType("string", noValidator, STRING_COLOR, TransferNothing),
   Undefined: new ColorType("undefined", obj => !CheckIsExist(obj), UNDEFINED_COLOR, TransferNothing)
 };
---
+++ src/helpers/color.ts
@@ -0,0 +1,31 @@
+import moment, { isMoment, isDate, Moment } from "moment";
+import { CheckIsNumber, MakeReadableNumberArray } from "./helper";
+
+/************************/
+/*       Transfer       */
+/************************/
+
+export const TransferNothing = (v: any) => v;
+export const TransferAsDate = (v: any) => {
+  const settings = {
+    sameDay: "[วันนี้]",
+    lastDay: "[เมื่อวาน]",
+    lastWeek: "[วัน]dddd[ที่แล้ว]",
+    sameElse: "DD/MM/YYYY"
+  };
+  if (isMoment(v)) return (<Moment>v).calendar(undefined, settings) || "";
+  else if (isDate(v)) return moment(<Date>v).calendar(undefined, settings) || "";
+  return v;
+};
+export const TransferReadableList = (v: Array<any>) => {
+  if (v.every(v => CheckIsNumber(v) !== null)) {
+    return MakeReadableNumberArray(v);
+  }
+  return v.toString();
+};
+
+/************************/
+/*       Validate       */
+/************************/
+
+export const noValidator = () => false;
