f917930b93fdf01ad08fac3bcac26b3bdb437a2f
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-11-07 16:15:43 +0100

    [fix] Warning should be in the end of script

--- src/apis/novel.ts
+++ src/apis/novel.ts
@@ -266,9 +266,7 @@ export const GetNovelContent = (chapter: NovelChapter, $: CheerioStatic) => {
 
   if (result.length < 1) {
     // log(WrapTM("debug", "The raw result", $.html()));
-    NOVEL_WARN.clone()
-      .loadString(`Cannot get ${chapter.id} chapter ${chapter.number} content`)
-      .printAndExit();
+    throw NOVEL_WARN.clone().loadString(`Cannot get ${chapter.id} chapter ${chapter.number} content`);
   }
 
   return result;
