8fd10ae691321d88c0d432d0b0984c9f7e96b5ee
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-11-25 02:22:09 +0100

    [doc] Make document to class interface (#25)

--- src/models/Observable.ts
+++ src/models/Observable.ts
@@ -5,28 +5,59 @@
 
 import { Observer } from "./Observer";
 
-// ผู้ถูกสังเกตการณ์
+/**
+ * Thai: ผู้ถูกสังเกตการณ์
+ * English: This Observable is the rxjs Observable concept, but the program need a little bit of custom, to make notify even easier to use.
+ *
+ * In the program I design that each Observable contain ONLY 1 observer.
+ * Basically, I design this class to notify when the changes was occurred.
+ *
+ * With additional feature, pause/unpause observe.
+ *
+ * If you don't temporary want to notify change to the observer, you can call {@link pauseObserve} to stop notify anything to observer.
+ * And you can resume be called {@link startObserve} method.
+ */
 export class Observable<T> {
   private observer?: Observer<T>;
 
   private collected = true;
 
+  /**
+   * Load observer, you can load observer in this subclass only.
+   * And the observer will overwrited if it already exist.
+   *
+   * @param observer observer
+   */
   protected loadObserver(observer: Observer<T>) {
     this.observer = observer;
   }
 
+  /**
+   * Notify change to observer
+   *
+   * @param result changes result
+   */
   public notify(result: T | undefined) {
     if (this.observer && this.collected) this.observer.notify(result);
   }
 
+  /**
+   * Get observer that current observe this class
+   */
   public observe() {
     return this.observer;
   }
 
+  /**
+   * pause will make observer cannot hear any changes
+   */
   public pauseObserve() {
     this.collected = false;
   }
 
+  /**
+   * start observe, you have to run this only if you pause it
+   */
   public startObserve() {
     this.collected = true;
   }
--- src/models/Printer.ts
+++ src/models/Printer.ts
@@ -3,11 +3,34 @@
  * @module public.object
  */
 
+/**
+ * This is a option type of printer interface.
+ */
 export interface PrintOption {
   short?: boolean;
 }
 
+/**
+ * The concept of the interface is, the class that able to interact with user must printable to show the result the user.
+ * so that this will have only 2 method, which is format (format the class as string), print (print the result to stdout)
+ *
+ * @author Kamontat Chantrachirathumrong
+ * @version 1.0.0
+ * @since November 25, 2018
+ */
 export interface Printer {
+  /**
+   * Format the information of the class and return as string
+   *
+   * @param opt print option
+   * @return formated string
+   */
   format(opt?: PrintOption): string;
+
+  /**
+   * Print the format (format in this case, NOT necessary to use {@link format} method) to stdout.
+   *
+   * @param opt print option
+   */
   print(opt?: PrintOption): void;
 }
