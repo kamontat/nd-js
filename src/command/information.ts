e97ce165b403a18a1f1093ef5fa76c52fea2f6c6
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-11-29 23:25:03 +0100

    [fix] Fix information type

--- src/command/information.ts
+++ src/command/information.ts
@@ -7,16 +7,21 @@ import { log } from "winston";
 
 import { VERSION } from "../../security/index-main";
 import { WrapTMCT } from "../apis/loggerWrapper";
+import { COLORS } from "../constants/color.const";
 import { ND } from "../constants/nd.const";
 // import { SeperateArgumentApi } from "../helpers/commander";
 
 export default (_: any) => {
   // const { options, args } = SeperateArgumentApi(a);
 
-  log(WrapTMCT("info", "ND official name", ND.PROJECT_NAME));
+  log(
+    WrapTMCT("info", "ND official name", ND.PROJECT_NAME, {
+      message: COLORS.Name,
+    }),
+  );
   log(WrapTMCT("info", "Main version", ND.VERSION));
   log(WrapTMCT("info", "Security version", VERSION));
 
-  log(WrapTMCT("info", "ND build time", ND.TIME));
+  log(WrapTMCT("info", "ND build time", ND.TIME, { message: COLORS.DateTime }));
   log(WrapTMCT("info", "ND state", ND.ENV));
 };
