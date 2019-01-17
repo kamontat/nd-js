ac0938271c0f495035ae901bfb4718d70ee42bed
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2019-01-17 14:35:29 +0700

    [refactor] format security code

--- security/lib/adminUtils.ts
+++ security/lib/adminUtils.ts
@@ -9,10 +9,13 @@ import { TokenManagement } from "./Token";
 
 const manager = new TokenManagement(Config.CONST);
 
-export const SignToken = (version: string, data: RequireTokenData) => manager.signToken(version, data);
+export const SignToken = (version: string, data: RequireTokenData) =>
+  manager.signToken(version, data);
 
 export const DecodeToken = (version: string, hex: string, fullname: string) =>
   manager.decodeToken(version, hex, fullname);
 
-export const ConvertToRequireTokenData = (result: ResultTokenData, fullname: string) =>
-  manager.convertToRequireTokenData(result, fullname);
+export const ConvertToRequireTokenData = (
+  result: ResultTokenData,
+  fullname: string,
+) => manager.convertToRequireTokenData(result, fullname);
