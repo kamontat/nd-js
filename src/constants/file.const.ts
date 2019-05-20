2ece2463594694212a34a1bb6b8b34514640ed9f
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2019-05-20 14:22:14 +0700

    [fix] create folder error sometime

--- src/constants/file.const.ts
+++ src/constants/file.const.ts
@@ -11,16 +11,16 @@ import { ND } from "./nd.const";
 
 export const DIR_ND_ROOT_NAME = ND.PROJECT_NAME;
 export const DIR_ND_ROOT_PATH = path.join(tmpdir(), DIR_ND_ROOT_NAME);
-if (!fs.existsSync(DIR_ND_ROOT_PATH)) fs.mkdirSync(DIR_ND_ROOT_PATH, { recursive: true });
+if (!fs.existsSync(DIR_ND_ROOT_PATH)) fs.mkdirSync(DIR_ND_ROOT_PATH);
 
 export const DIR_TMP_NAME = "tmp";
 export const DIR_TMP_PATH = path.join(DIR_ND_ROOT_PATH, DIR_TMP_NAME);
-if (!fs.existsSync(DIR_TMP_PATH)) fs.mkdirSync(DIR_TMP_PATH, { recursive: true });
+if (!fs.existsSync(DIR_TMP_PATH)) fs.mkdirSync(DIR_TMP_PATH);
 
 export const DIR_TMP_RANDOM = fs.mkdtempSync(path.join(DIR_TMP_PATH, "nd-"));
-if (!fs.existsSync(DIR_TMP_RANDOM)) fs.mkdirSync(DIR_TMP_RANDOM, { recursive: true });
+if (!fs.existsSync(DIR_TMP_RANDOM)) fs.mkdirSync(DIR_TMP_RANDOM);
 
 // TODO: change log to this folder
 export const DIR_LOG_NAME = "logs";
 export const DIR_LOG_PATH = path.join(DIR_ND_ROOT_PATH, DIR_LOG_NAME);
-if (!fs.existsSync(DIR_LOG_PATH)) fs.mkdirSync(DIR_LOG_PATH, { recursive: true });
+if (!fs.existsSync(DIR_LOG_PATH)) fs.mkdirSync(DIR_LOG_PATH);
